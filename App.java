29594a6e9f6c25cfd17a7f72c144e1438f672d16
M. ATSAL HAFIDZ <114473177+atsalhfdz5@users.noreply.github.com>
2024-03-21 19:44:26 +0700

    Add files via upload

---
+++ App.java
@@ -0,0 +1,10 @@
+
+public class App{
+    public static void main(String[] args){
+        Mahasiswa mahasiswa = new Mahasiswa(
+            "M. Atsal Hafidz",
+            2004,
+            "Teknik Informatika");
+        System.out.println(mahasiswa);
+    }
+}
